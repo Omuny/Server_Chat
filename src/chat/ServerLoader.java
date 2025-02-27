4950e4d0ce5dc060048d34c45e6b903b6e505e3c
omuny <omuny@yandex.ru>
2025-02-27 22:21:31 +0300

    Создание сервер сокета. Обработка подключения клиентов.

--- src/chat/ServerLoader.java
+++ src/chat/ServerLoader.java
@@ -1,7 +1,48 @@
 package chat;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerLoader {
+
+    private static ServerSocket server;
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        start();
+        handle();
+        end();
     }
+
+    private  static void handle() {
+        while (true) {
+            // Обработка подключающихся клиентов
+            try {
+                Socket client = server.accept();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException ex) {}
+        }
+    }
+
+    public static void start() {
+        try {
+            server = new ServerSocket(1446);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void end() {
+        try {
+            server.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
 }
